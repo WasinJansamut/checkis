ed74168186d085a4113d5c87517cc121e11761f6
WasinJansamut <wasinjansamut@gmail.com>
2025-05-28 12:29:44 +0700

    Refactor User model to use $guarded instead of $fillable for mass assignment

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,17 +12,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'username',
-        'usercode',
-        'password',
-        'firstname',
-        'lastname',
-        'phone',
-    ];
-
+    protected $guarded = [];
     /**
      * The attributes that should be hidden for serialization.
      *
