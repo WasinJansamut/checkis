1122ad716b2b5394ba1fedc54b280a4dbb76599d
WasinJansamut <wasinjansamut@gmail.com>
2025-06-08 20:44:27 +0700

    check no 2

--- resources/views/exports/case.blade.php
+++ resources/views/exports/case.blade.php
@@ -1,31 +1,37 @@
-    <table>
-        <thead>
+<table>
+    <thead>
 
-            <tr>
+        <tr>
+            @if ($isFirstSheet)
                 <th style="background-color: yellow; font-weight: bold;">ตัวแปรที่ว่าง</th>
+            @endif
+            @foreach ($header as $col)
+                @if (in_array($col, $highlight_columns))
+                    <td style="background-color: yellow">{{ $col }}</td>
+                @else
+                    <td>{{ $col }}</td>
+                @endif
+            @endforeach
+
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($isData as $row)
+            <tr>
+                @if ($isFirstSheet)
+                    <td style="background-color: yellow; font-weight: bold;">
+                        {{ isset($emptyFields[$row->id]) ? implode(', ', $emptyFields[$row->id]) : '' }}
+                    </td>
+                @endif
                 @foreach ($header as $col)
                     @if (in_array($col, $highlight_columns))
-                        <td style="background-color: yellow">{{ $col }}</td>
+                        <td style="background-color: yellow">{{ $row->{$col} }}</td>
                     @else
-                        <td>{{ $col }}</td>
+                        <td>{{ $row->{$col} }}</td>
                     @endif
                 @endforeach
 
             </tr>
-        </thead>
-        <tbody>
-            @foreach ($isData as $row)
-                <tr>
-                    <td style="background-color: yellow; font-weight: bold;">{{ isset($emptyFields[$row->id]) ? implode(', ', $emptyFields[$row->id]) : '' }}</td>
-                    @foreach ($header as $col)
-                        @if (in_array($col, $highlight_columns))
-                            <td style="background-color: yellow">{{ $row->{$col} }}</td>
-                        @else
-                            <td>{{ $row->{$col} }}</td>
-                        @endif
-                    @endforeach
-
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+        @endforeach
+    </tbody>
+</table>
